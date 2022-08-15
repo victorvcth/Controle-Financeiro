dcd4e8f95b5aebfd21a6f8d51c7f9fbba610d02a
Victor Barros <victor_buzetti@hotmail.com>
2022-08-15 19:10:45 -0300

    Primeiro commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import * as C from "./style"
+
+const header = () => {
+    return (
+        <C.Container> 
+
+        <C.Header>
+            <C.Title>Controle Financeiro</C.Title>
+        </C.Header>
+
+        </C.Container>
+    )
+}
+
+export default header
